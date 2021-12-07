f2058f69e144b45256d5cb3988e5bdcf77b666ab
David Brennan <davidjabrennan@gmail.com>
2021-12-07 12:07:04 +0000

    Day 7 Part 1

---
+++ day07/Day 7 Challenge 1.py
@@ -0,0 +1,16 @@
+from functools import reduce
+
+file = "input.txt"
+
+crabs = [int(x) for x in open(file).read().split(",")]
+lowest = min(crabs)
+highest = max(crabs)
+total = float('inf')
+chosen_position = 0
+
+for option in range(lowest, highest + 1):
+    this_total = sum([abs(x - option) for x in crabs])
+    if this_total < total:
+        total = this_total
+        chosen_position = option
+print(total)
